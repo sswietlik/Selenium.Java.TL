726defb0b26d8994b365f05818668cb3483113aa
sswietlik <sswietlik@gmail.com>
2020-01-25 22:13:38 +0100

    Koniec zadania 2

--- src/test/java/zadania/zadanie_2.java
+++ src/test/java/zadania/zadanie_2.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -26,23 +27,37 @@ public class zadanie_2 {
     }
 
     @Test
-    public void subTest1() {
-        String pageTitleExpected = "Wikipedia, wolna encyklopedia";
+    public void subTest_1(){
         driver.get("https://pl.wikipedia.org");
-        Assertions.assertEquals(pageTitleExpected, driver.getTitle(), "Page title is not equal" + pageTitleExpected);
-    }
-     @Test
-        public void subTest2(){
+        String pageTitle = "Wikipedia, wolna encyklopedia";
         String  expectedURL = "https://pl.wikipedia.org/";
-        driver.navigate().to("https://pl.wikipedia.org/");
-        Assertions.assertEquals(expectedURL.contains("wikipedia.org/"), driver.getCurrentUrl().contains("pl.wikipedia.org"),"not" +expectedURL);
-    }
-    @Test
-    public void subTest3(){
-        String actPageLang = "lang=\"pl\"";
-        driver.navigate().to("pl.wikipedia.org");
-        Assertions.assertTrue(driver.getPageSource().contains(actPageLang), "lang is not:" +actPageLang);
-    }
 
+        Assertions.assertEquals(pageTitle, driver.getTitle(), "Page is not" +pageTitle);
+        System.out.println("Assertion_1 "+driver.getTitle());
+
+        Assertions.assertTrue(expectedURL.contains("pl.wikipedia"),driver.getCurrentUrl());
+        System.out.println("Assertion_2 "+driver.getCurrentUrl());
+
+        Assertions.assertTrue(driver.getPageSource().contains("lang=\"pl\""));
+        System.out.println("Assertion_3");
+
+        driver.findElement(By.cssSelector("a[lang='es']")).click();
+        System.out.println("Lang swap ");
+
+
+        String pageTitle2 = "Wikipedia, la enciclopedia libre";
+        String  expectedURL2 = "https://es.wikipedia.org/";
+
+        Assertions.assertEquals(pageTitle2, driver.getTitle(), "Page is not" +pageTitle2);
+        System.out.println("Assertion_4 "+driver.getTitle());
+
+        Assertions.assertTrue(expectedURL2.contains("es.wikipedia"),driver.getCurrentUrl());
+        System.out.println("Assertion_5 "+driver.getCurrentUrl());
+
+        Assertions.assertTrue(driver.getPageSource().contains("lang=\"es\""));
+        System.out.println("Assertion_6");
+
+
+    }
 
 }
